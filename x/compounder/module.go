e0003078a6cb433c588a2504ca9dcdbe4381ee49
george-aj <35270943+george-aj@users.noreply.github.com>
2023-05-04 09:52:05 -0500

    Add compounding runner to end block

--- x/compounder/module.go
+++ x/compounder/module.go
@@ -154,6 +154,11 @@ func (AppModule) ConsensusVersion() uint64 { return 1 }
 func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
 
 // EndBlock contains the logic that is automatically triggered at the end of each block
-func (am AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+	err := am.keeper.RunCompounding(ctx)
+	if err != nil {
+		panic(err)
+	}
+
 	return []abci.ValidatorUpdate{}
 }
