35c1499c9581b0d5a3eec410c125aad206a2007d
george-aj <35270943+george-aj@users.noreply.github.com>
2023-06-14 16:09:37 -0500

    create connection

--- x/icayieldmos/keeper/msg_server_create_connection.go
+++ x/icayieldmos/keeper/msg_server_create_connection.go
@@ -10,8 +10,10 @@ import (
 func (k msgServer) CreateConnection(goCtx context.Context, msg *types.MsgCreateConnection) (*types.MsgCreateConnectionResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	err := k.RegisterInterchainAccount(ctx, msg.ConnectionId)
+	if err != nil {
+		return &types.MsgCreateConnectionResponse{}, err
+	}
 
 	return &types.MsgCreateConnectionResponse{}, nil
 }
