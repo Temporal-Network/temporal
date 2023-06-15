33029c2249847d515f605044a1c3dac69ee63cb1
george-aj <35270943+george-aj@users.noreply.github.com>
2023-06-15 16:16:21 -0500

    url clean up

--- x/icayieldmos/types/query.pb.gw.go
+++ x/icayieldmos/types/query.pb.gw.go
@@ -323,9 +323,9 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 var (
 	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"temporal", "icayieldmos", "params"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_ContractRemoteZone_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"Temporal-Network", "temporal", "icayieldmos", "contract_remote_zone", "id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_ContractRemoteZone_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"temporal", "icayieldmos", "contract_remote_zone", "id"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_ContractRemoteZoneAll_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"Temporal-Network", "temporal", "icayieldmos", "contract_remote_zone"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_ContractRemoteZoneAll_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"temporal", "icayieldmos", "contract_remote_zone"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
