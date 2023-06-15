7e330aaf1e718f5cb27665ad60953508614ae2cb
george-aj <35270943+george-aj@users.noreply.github.com>
2023-06-15 15:13:50 -0500

    contract remote zone validation logic

--- x/icayieldmos/keeper/msg_server_contract_remote_zone.go
+++ x/icayieldmos/keeper/msg_server_contract_remote_zone.go
@@ -1,75 +1,113 @@
 package keeper
 
 import (
-    "fmt"
 	"context"
+	"fmt"
 
-    "github.com/Temporal-Network/temporal/x/icayieldmos/types"
+	"github.com/Temporal-Network/temporal/x/icayieldmos/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+func (k msgServer) isContractRemoteZoneValid(ctx sdk.Context, connectionId string, remoteChainId string) error {
+	owner := k.accountKeeper.GetModuleAddress(types.ModuleName)
 
-func (k msgServer) CreateContractRemoteZone(goCtx context.Context,  msg *types.MsgCreateContractRemoteZone) (*types.MsgCreateContractRemoteZoneResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-    var contractRemoteZone = types.ContractRemoteZone{
-        Creator: msg.Creator,
-        ConnectionId: msg.ConnectionId,
-        RemoteChainId: msg.RemoteChainId,
-        Active: msg.Active,
-    }
-
-    id := k.AppendContractRemoteZone(
-        ctx,
-        contractRemoteZone,
-    )
-
-	return &types.MsgCreateContractRemoteZoneResponse{
-	    Id: id,
-	}, nil
-}
-
-func (k msgServer) UpdateContractRemoteZone(goCtx context.Context,  msg *types.MsgUpdateContractRemoteZone) (*types.MsgUpdateContractRemoteZoneResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-    var contractRemoteZone = types.ContractRemoteZone{
-		Creator: msg.Creator,
-		Id:      msg.Id,
-    	ConnectionId: msg.ConnectionId,
-    	RemoteChainId: msg.RemoteChainId,
-    	Active: msg.Active,
+	if !k.DoesInterchainAccountExist(ctx, connectionId, owner.String()) {
+		return sdkerrors.Wrap(sdkerrors.ErrNotFound, fmt.Sprintf("ica account not found for connectionId: %s and owner: %s", connectionId, owner.String()))
 	}
 
-    // Checks that the element exists
-    val, found := k.GetContractRemoteZone(ctx, msg.Id)
-    if !found {
-        return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
-    }
+	if !k.IsChannelOpen(ctx, connectionId, owner.String()) {
+		return sdkerrors.Wrap(sdkerrors.ErrNotFound, fmt.Sprintf("channel not open for connectionId: %s and owner: %s", connectionId, owner.String()))
+	}
 
-    // Checks if the msg creator is the same as the current owner
-    if msg.Creator != val.Creator {
-        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
-    }
+	_, found := k.GetRemoteModuleAddress(ctx, connectionId, owner.String())
+	if !found {
+		return sdkerrors.Wrap(sdkerrors.ErrNotFound, fmt.Sprintf("remote address not found for connectionId: %s and owner: %s", connectionId, owner.String()))
+	}
+
+	remoteZones := k.GetAllContractRemoteZone(ctx)
+
+	for _, remoteZone := range remoteZones {
+		if connectionId == remoteZone.ConnectionId {
+			return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, fmt.Sprintf("duplicate remote zone not allowed for connectionId: %s", connectionId))
+		}
+
+		if remoteChainId == remoteZone.RemoteChainId {
+			return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, fmt.Sprintf("duplicate remote zone not allowed for remoteChainId: %s", remoteChainId))
+		}
+	}
+
+	return nil
+}
+
+// TODO Restrict who can create ContracRemoteZones
+func (k msgServer) CreateContractRemoteZone(goCtx context.Context, msg *types.MsgCreateContractRemoteZone) (*types.MsgCreateContractRemoteZoneResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	err := k.isContractRemoteZoneValid(ctx, msg.ConnectionId, msg.RemoteChainId)
+	if err != nil {
+		return &types.MsgCreateContractRemoteZoneResponse{}, err
+	}
+
+	var contractRemoteZone = types.ContractRemoteZone{
+		Creator:       msg.Creator,
+		ConnectionId:  msg.ConnectionId,
+		RemoteChainId: msg.RemoteChainId,
+		Active:        msg.Active,
+	}
+
+	id := k.AppendContractRemoteZone(ctx, contractRemoteZone)
+
+	return &types.MsgCreateContractRemoteZoneResponse{Id: id}, nil
+}
+
+// TODO Restrict who can update ContracRemoteZones
+func (k msgServer) UpdateContractRemoteZone(goCtx context.Context, msg *types.MsgUpdateContractRemoteZone) (*types.MsgUpdateContractRemoteZoneResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	err := k.isContractRemoteZoneValid(ctx, msg.ConnectionId, msg.RemoteChainId)
+	if err != nil {
+		return &types.MsgUpdateContractRemoteZoneResponse{}, err
+	}
+
+	var contractRemoteZone = types.ContractRemoteZone{
+		Creator:       msg.Creator,
+		Id:            msg.Id,
+		ConnectionId:  msg.ConnectionId,
+		RemoteChainId: msg.RemoteChainId,
+		Active:        msg.Active,
+	}
+
+	// Checks that the element exists
+	val, found := k.GetContractRemoteZone(ctx, msg.Id)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+
+	// Checks if the msg creator is the same as the current owner
+	if msg.Creator != val.Creator {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
 
 	k.SetContractRemoteZone(ctx, contractRemoteZone)
 
 	return &types.MsgUpdateContractRemoteZoneResponse{}, nil
 }
 
-func (k msgServer) DeleteContractRemoteZone(goCtx context.Context,  msg *types.MsgDeleteContractRemoteZone) (*types.MsgDeleteContractRemoteZoneResponse, error) {
+// TODO Restrict who can delete ContracRemoteZones
+func (k msgServer) DeleteContractRemoteZone(goCtx context.Context, msg *types.MsgDeleteContractRemoteZone) (*types.MsgDeleteContractRemoteZoneResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-    // Checks that the element exists
-    val, found := k.GetContractRemoteZone(ctx, msg.Id)
-    if !found {
-        return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
-    }
+	// Checks that the element exists
+	val, found := k.GetContractRemoteZone(ctx, msg.Id)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
 
-    // Checks if the msg creator is the same as the current owner
-    if msg.Creator != val.Creator {
-        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
-    }
+	// Checks if the msg creator is the same as the current owner
+	if msg.Creator != val.Creator {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
 
 	k.RemoveContractRemoteZone(ctx, msg.Id)
 
