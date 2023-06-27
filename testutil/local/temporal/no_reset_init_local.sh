278506cd947004fccdc21ae0687c34ef7136e2f6
George <35270943+george-aj@users.noreply.github.com>
2023-06-27 12:26:27 -0500

    local build changes

--- testutil/local/temporal/no_reset_init_local.sh
+++ testutil/local/temporal/no_reset_init_local.sh
@@ -3,4 +3,4 @@
 CMD="temporald"
 DENOM="utprl"
 
-$CMD start --grpc.address 0.0.0.0:10090 --pruning nothing --p2p.laddr 0.0.0.0:36656 --address 0.0.0.0:36658 --rpc.laddr tcp://0.0.0.0:36657 --rpc.pprof_laddr 0.0.0.0:7060 --api.address tcp://0.0.0.0:2317 --grpc-web.address 0.0.0.0:10091
+$CMD start --grpc.address 0.0.0.0:10090 --pruning nothing --p2p.laddr 0.0.0.0:36656 --address 0.0.0.0:36658 --rpc.laddr tcp://0.0.0.0:36657 --rpc.pprof_laddr 0.0.0.0:7060 --api.address tcp://0.0.0.0:2317 --api.enable true --grpc-web.address 0.0.0.0:10091
--- testutil/local/temporal/reset_init_local.sh
+++ testutil/local/temporal/reset_init_local.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-rm -r ~/.temporald/keyring-test/
-rm -r ~/.temporald/config/gentx/
-rm  ~/.temporal/config/gentx/gentx-30c040a4b9ca0d9bc3e62efd2ded9c860a8df799.json
-
-VAL_1_MNEMONIC="ocean toe void inquiry skin exotic artist teach daughter theory melt theme desk more trip person maid agent mesh prefer loop oyster member exact"
-VAL_1_ADDRESS="temporal1nhvm23r9pu593sgdu660whdz66pg382czn5quy"
-
 CMD="temporald"
 DENOM="utprl"
 CHAIN_ID="temporal"
 
 $CMD tendermint unsafe-reset-all
 $CMD tendermint reset-state
+
+rm -rf ~/.temporald/keyring-test/
+rm -rf ~/.temporald/config/gentx/
+rm  -rf ~/.temporal/
+
+make dev-install
+
+VAL_1_MNEMONIC="ocean toe void inquiry skin exotic artist teach daughter theory melt theme desk more trip person maid agent mesh prefer loop oyster member exact"
+VAL_1_ADDRESS="temporal1nhvm23r9pu593sgdu660whdz66pg382czn5quy"
+
+$CMD config keyring-backend test
+$CMD config node tcp://127.0.0.1:36657
 
 echo "$VAL_1_MNEMONIC" | $CMD keys add $VAL_1_ADDRESS --recover --keyring-backend=test 2>&1
 
@@ -24,4 +29,8 @@ $CMD gentx $VAL_1_ADDRESS 1000000$DENOM --chain-id $CHAIN_ID --keyring-backend t
 
 $CMD collect-gentxs
 
-$CMD start
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPT=$SCRIPTPATH"/no_reset_init_local.sh"
+chmod +x $SCRIPT
+
+sh $SCRIPT
